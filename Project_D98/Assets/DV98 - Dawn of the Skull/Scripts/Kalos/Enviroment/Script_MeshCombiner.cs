e36ff03497d0f82fa42bedcb5e8a155dd92b1c08
Kalos Kalazos <carlos.csm44@gmail.com>
2024-10-23 13:26:52 +0200

    Fix Mesh combiner
    +now we create a mesh combined asset instead of generate this process on runtime

--- Project_D98/Assets/DV98 - Dawn of the Skull/Scripts/Kalos/Enviroment/Script_MeshCombiner.cs
+++ Project_D98/Assets/DV98 - Dawn of the Skull/Scripts/Kalos/Enviroment/Script_MeshCombiner.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 
-[RequireComponent(typeof(MeshFilter))]
-[RequireComponent(typeof(MeshRenderer))]
 
 public class Script_MeshCombiner : MonoBehaviour
 {
+    [SerializeField] private List<MeshFilter> listMeshFilter;
+
+    [SerializeField] private MeshFilter TargetMesh;
+
 
     void Start()
     {
-        CombineMesh();
 
     }
 
@@ -19,30 +21,71 @@ public class Script_MeshCombiner : MonoBehaviour
     {
     }
 
-    void CombineMesh()
-    {
+    [ContextMenu("Combine Meshes")]
 
-        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
-        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+    private void CombineMesh()
+    {        
+       CombineInstance[] combine = new CombineInstance[listMeshFilter.Count];
 
-        int i = 0;
-        while (i < meshFilters.Length)
+        for (int i = 0; i < listMeshFilter.Count; i++)
         {
-            combine[i].mesh = meshFilters[i].sharedMesh;
-            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
-            meshFilters[i].gameObject.SetActive(false);
-
-            i++;
+            combine[i].mesh = listMeshFilter[i].sharedMesh;
+            combine[i].transform = listMeshFilter[i].transform.localToWorldMatrix;
+            listMeshFilter[i].gameObject.SetActive(false);
         }
 
-        var meshFilter = transform.GetComponent<MeshFilter>();
-        meshFilter.mesh = new Mesh();
-        meshFilter.mesh.CombineMeshes(combine);
-        transform.GetComponent<MeshFilter>().sharedMesh = transform.GetComponent<MeshFilter>().mesh;
-        GetComponent<MeshFilter>().sharedMesh = meshFilter.mesh;
-        transform.gameObject.SetActive(true);
+        
 
-        transform.rotation = Quaternion.identity;
-        transform.position = Vector3.zero;
+        var mesh = new Mesh(); 
+        mesh.CombineMeshes(combine);
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        TargetMesh.mesh = mesh;
+
+        SaveMesh(TargetMesh.sharedMesh, gameObject.name, false, true);
     }
+
+
+    public static void SaveMesh(Mesh mesh, string name, bool makeNewInstance, bool optimizeMesh)
+    {
+        string path = EditorUtility.SaveFilePanel("Save Separate Mesh Asset", "Assets/", name, "asset");
+        if (string.IsNullOrEmpty(path)) return;
+
+        path = FileUtil.GetProjectRelativePath(path);
+
+        Mesh meshToSave = (makeNewInstance) ? Object.Instantiate(mesh) as Mesh : mesh;
+
+        if(optimizeMesh) MeshUtility.Optimize(meshToSave);
+
+        AssetDatabase.CreateAsset(meshToSave, path);
+        AssetDatabase.SaveAssets();
+    }
+
+
+    //void CombineMesh()
+   // {
+    //    CombineInstance[] combine = new CombineInstance[listMeshFilter.Count];
+//
+    //    for (int i = 0; i < listMeshFilter.Count; i++)
+   //     {
+   //         combine[i].mesh = listMeshFilter[i].sharedMesh;
+   //         combine[i].transform = listMeshFilter[i].transform.localToWorldMatrix;
+   //         listMeshFilter[i].gameObject.SetActive(false);
+   //     }
+//
+   //     var meshFilter = transform.GetComponent<MeshFilter>();
+   //     meshFilter.mesh = new Mesh();
+   //     meshFilter.mesh.CombineMeshes(combine);
+//
+ //       meshFilter.mesh.RecalculateBounds();
+//
+  //      meshFilter.mesh.RecalculateNormals();
+//
+ //       transform.GetComponent<MeshFilter>().sharedMesh = transform.GetComponent<MeshFilter>().mesh;
+   //     GetComponent<MeshFilter>().sharedMesh = meshFilter.mesh;
+  //      transform.gameObject.SetActive(true);
+//
+    //    transform.rotation = Quaternion.identity;
+    //    transform.position = Vector3.zero;
+    //}
 }

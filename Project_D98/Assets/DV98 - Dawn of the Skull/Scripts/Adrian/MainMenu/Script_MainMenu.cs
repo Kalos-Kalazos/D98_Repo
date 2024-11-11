306d2e348aa9553e34b3ec28449bd710ec3c876c
adridev <73952455+LAHOZ99@users.noreply.github.com>
2024-11-11 21:00:36 +0100

    MainMenu Updated

--- Project_D98/Assets/DV98 - Dawn of the Skull/Scripts/Adrian/MainMenu/Script_MainMenu.cs
+++ Project_D98/Assets/DV98 - Dawn of the Skull/Scripts/Adrian/MainMenu/Script_MainMenu.cs
@@ -10,14 +10,19 @@ public class Script_MainMenu : MonoBehaviour
         Script_AudioManager.Instance.PlayMusic(0);
     }
 
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void OptionsGame()
     {
-        SceneManager.LoadScene("OptionsGame");
+        SceneManager.LoadScene("OptionsScene");
     }
 
     public void Quit()
